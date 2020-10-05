9e068aed7a8d2e623e2f9b891f90ce95194d283d
Roman Lipovsky <roman.lipovsky@gmail.com>
2020-10-05 16:28:17 +0300

    disable logging in release build

--- whirl/node/logging.hpp
+++ whirl/node/logging.hpp
@@ -4,6 +4,10 @@
 
 namespace whirl {
 
-#define NODE_LOG(...) NodeLogger()->Log(fmt::format(__VA_ARGS__));
+#ifndef NDEBUG
+#define NODE_LOG(...) NodeLogger()->Log(fmt::format(__VA_ARGS__))
+#else
+#define NODE_LOG(...)
+#endif
 
 }  // namespace whirl
