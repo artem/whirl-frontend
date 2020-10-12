fa8ed03c8fbc00033b2b2dbb7ed40a1b7c6da0a6
Roman Lipovsky <roman.lipovsky@gmail.com>
2020-10-12 13:31:31 +0300

    more random timings

--- whirl/matrix/world/dice.hpp
+++ whirl/matrix/world/dice.hpp
@@ -7,19 +7,26 @@
 
 namespace whirl {
 
-// Local clocks
+// Clocks
 
 inline TimePoint ResetMonotonicClock() {
   return GlobalRandomNumber(1, 100);
 }
 
 inline TimePoint InitLocalClockOffset() {
-  return GlobalRandomNumber(100);
+  return GlobalRandomNumber(1000);
+}
+
+inline Duration TrueTimeUncertainty() {
+  return GlobalRandomNumber(5, 500);
 }
 
 // Network
 
 inline TimePoint NetPacketDeliveryTime() {
+  if (GlobalRandomNumber() % 5 == 0) {
+    return GlobalRandomNumber(10, 1000);
+  }
   return GlobalRandomNumber(30, 60);
 }
 
