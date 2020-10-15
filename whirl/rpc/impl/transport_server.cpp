c3178df03bccfbe337be34e357769ee8ab98e3a7
Roman Lipovsky <roman.lipovsky@gmail.com>
2020-10-16 00:15:29 +0300

    better

--- whirl/rpc/impl/transport_server.cpp
+++ whirl/rpc/impl/transport_server.cpp
@@ -1,7 +1,81 @@
 #include <whirl/rpc/impl/transport_server.hpp>
 
+#include <whirl/rpc/impl/trace.hpp>
+
+#include <await/fibers/core/api.hpp>
+
+#include <wheels/support/exception.hpp>
+
 namespace whirl::rpc {
 
-// ...
+void RPCTransportServer::Start() {
+  server_ = transport_->Serve(shared_from_this());
+}
+
+void RPCTransportServer::RegisterMethod(const std::string& method, RPCMethodInvoker invoker) {
+  if (methods_.find(method) != methods_.end()) {
+    WHEELS_PANIC("RPC method '" << method << "' already registered");
+  }
+
+  methods_.emplace(method, std::move(invoker));
+}
+
+void RPCTransportServer::Shutdown() {
+  if (server_) {
+    server_->Shutdown();
+  }
+}
+
+// ITransportHandler
+
+void RPCTransportServer::HandleMessage(const TransportMessage& message,
+                   ITransportSocketPtr back) {
+  // Process request
+  await::fibers::Spawn(
+      [self = shared_from_this(), message, back = std::move(back)]() mutable {
+        self->ProcessRequest(message, back);
+      },
+      executor_);
+}
+
+void RPCTransportServer::HandleDisconnect() {
+  // Some client lost
+}
+
+
+void RPCTransportServer::ProcessRequest(const TransportMessage& message,
+                    const ITransportSocketPtr& back) {
+  auto request = Deserialize<RPCRequestMessage>(message);
+
+  SetThisFiberTraceId(request.trace_id);
+
+  WHIRL_FMT_LOG("Process method '{}' request with id = {}", request.method,
+                request.id);
+
+  if (methods_.count(request.method) == 0) {
+    ResponseWithError(request, back, RPCErrorCode::MethodNotFound);
+  }
+
+  BytesValue result;
+  auto invoker = methods_[request.method];
+  try {
+    result = invoker(request.input);
+  } catch (...) {
+    WHIRL_FMT_LOG("Exception in method '{}': {}", request.method,
+                  wheels::CurrentExceptionMessage());
+    ResponseWithError(request, back, RPCErrorCode::ExecutionError);
+  }
+  SendResponse({request.id, request.method, result, RPCErrorCode::Ok}, back);
+}
+
+void RPCTransportServer::ResponseWithError(const RPCRequestMessage& request,
+                       const ITransportSocketPtr& back, RPCErrorCode error) {
+  SendResponse({request.id, request.method, "", error}, back);
+}
+
+void RPCTransportServer::SendResponse(RPCResponseMessage response,
+                  const ITransportSocketPtr& back) {
+  back->Send(Serialize(response));
+}
 
 }  // namespace whirl::rpc
--- whirl/rpc/impl/transport_server.hpp
+++ whirl/rpc/impl/transport_server.hpp
@@ -3,12 +3,8 @@
 #include <whirl/rpc/impl/raw_value.hpp>
 #include <whirl/rpc/impl/protocol.hpp>
 #include <whirl/rpc/impl/net_transport.hpp>
-#include <whirl/rpc/impl/trace.hpp>
 
 #include <await/executors/executor.hpp>
-#include <await/fibers/core/api.hpp>
-
-#include <wheels/support/exception.hpp>
 
 // TODO: abstract logger
 #include <whirl/matrix/log/log.hpp>
@@ -37,76 +33,27 @@ class RPCTransportServer
       : transport_(t), executor_(e) {
   }
 
-  void Start() {
-    server_ = transport_->Serve(shared_from_this());
-  }
-
-  void RegisterMethod(const std::string& method, RPCMethodInvoker invoker) {
-    if (methods_.find(method) != methods_.end()) {
-      WHEELS_PANIC("RPC method '" << method << "' already registered");
-    }
-
-    methods_.emplace(method, std::move(invoker));
-  }
-
-  void Shutdown() {
-    if (server_) {
-      server_->Shutdown();
-    }
-  }
+  void Start();
+  void RegisterMethod(const std::string& method, RPCMethodInvoker invoker);
+  void Shutdown();
 
   // ITransportHandler
 
   void HandleMessage(const TransportMessage& message,
-                     ITransportSocketPtr back) override {
-    // Process request
-    await::fibers::Spawn(
-        [self = shared_from_this(), message, back = std::move(back)]() mutable {
-          self->ProcessRequest(message, back);
-        },
-        executor_);
-  }
+                     ITransportSocketPtr back) override;
 
-  void HandleDisconnect() override {
-    // Some client lost
-  }
+  void HandleDisconnect() override;
 
  private:
   // In separate fiber
   void ProcessRequest(const TransportMessage& message,
-                      const ITransportSocketPtr& back) {
-    auto request = Deserialize<RPCRequestMessage>(message);
-
-    SetThisFiberTraceId(request.trace_id);
-
-    WHIRL_FMT_LOG("Process method '{}' request with id = {}", request.method,
-                  request.id);
-
-    if (methods_.count(request.method) == 0) {
-      ResponseWithError(request, back, RPCErrorCode::MethodNotFound);
-    }
-
-    BytesValue result;
-    auto invoker = methods_[request.method];
-    try {
-      result = invoker(request.input);
-    } catch (...) {
-      WHIRL_FMT_LOG("Exception in method '{}': {}", request.method,
-                    wheels::CurrentExceptionMessage());
-      ResponseWithError(request, back, RPCErrorCode::ExecutionError);
-    }
-    SendResponse({request.id, request.method, result, RPCErrorCode::Ok}, back);
-  }
+                      const ITransportSocketPtr& back);
 
   void ResponseWithError(const RPCRequestMessage& request,
-                         const ITransportSocketPtr& back, RPCErrorCode error) {
-    SendResponse({request.id, request.method, "", error}, back);
-  }
+                         const ITransportSocketPtr& back, RPCErrorCode error);
 
   void SendResponse(RPCResponseMessage response,
-                    const ITransportSocketPtr& back) {
-    back->Send(Serialize(response));
-  }
+                    const ITransportSocketPtr& back);
 
  private:
   // Services
