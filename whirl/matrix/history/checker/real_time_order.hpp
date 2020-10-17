457d233e2ef88270f6aded14204ad198e1caa713
Roman Lipovsky <roman.lipovsky@gmail.com>
2020-10-17 16:31:35 +0300

    cosmetics

--- whirl/matrix/history/checker/real_time_order.hpp
+++ whirl/matrix/history/checker/real_time_order.hpp
@@ -6,7 +6,7 @@ namespace whirl::histories {
 
 // See Linearizability consistency model
 inline bool PrecedesInRealTime(const Call& lhs, const Call& rhs) {
-  return lhs.IsCompleted() && lhs.end_time < rhs.start_time;
+  return lhs.IsCompleted() && lhs.end_time.value() < rhs.start_time;
 }
 
 }  // namespace whirl::histories
