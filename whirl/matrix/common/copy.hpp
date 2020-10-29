f23f5230d16a7e42c936bca6d4baea7c2c3e1b52
Roman Lipovsky <roman.lipovsky@gmail.com>
2020-10-30 02:48:57 +0300

    warning

--- whirl/matrix/common/copy.hpp
+++ whirl/matrix/common/copy.hpp
@@ -4,6 +4,12 @@
 
 namespace whirl {
 
+// Warning:
+// Do not pass CoW-ed objects through heap boundaries
+// Only objects that make deep copy in copy ctor are allowed
+// Prefer primitive types, strings and structures
+
+
 // Use at boundaries of process/global heaps
 
 template <typename T>
@@ -13,9 +19,9 @@ T MakeCopy(const T& object) {
 }
 
 template <typename T>
-T CopyToHeap(const T& obj, Heap* heap) {
+T CopyToHeap(const T& object, Heap* heap) {
   HeapScopeGuard g(heap);
-  T copy{obj};
+  T copy{object};
   return copy;
 }
 
