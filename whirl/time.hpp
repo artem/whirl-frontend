6175b2bc6b7cff455bd5e65c472c8d6d1a0d10f5
Roman Lipovsky <roman.lipovsky@gmail.com>
2020-11-30 00:51:47 +0300

    comment

--- whirl/time.hpp
+++ whirl/time.hpp
@@ -4,7 +4,9 @@
 
 namespace whirl {
 
-// Microseconds?
+// Time measured in some abstract `jiffies` - Paxon unit of time
+// https://lamport.azurewebsites.net/pubs/lamport-paxos.pdf, page 13, footnote 11
+
 using TimePoint = size_t;
 
 using Duration = size_t;
