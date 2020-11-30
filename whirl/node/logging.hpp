414082c9cb9c99e122ae5a08475f1e4734f9cd44
Roman Lipovsky <roman.lipovsky@gmail.com>
2020-12-01 01:26:49 +0300

    support levels in NODE_LOG

--- whirl/node/logging.hpp
+++ whirl/node/logging.hpp
@@ -7,9 +7,21 @@
 namespace whirl {
 
 #ifndef NDEBUG
-#define NODE_LOG(...) NodeLogger()->Log(NodeLogLevel::Info, fmt::format(__VA_ARGS__))
+
+#define NODE_LOG_IMPL(level, ...) \
+  NodeLogger()->Log(level, fmt::format(__VA_ARGS__))
+
 #else
-#define NODE_LOG(...)
+
+#define NODE_LOG_IMPL(level, ...)
+
 #endif
+
+#define NODE_LOG_DEBUG(...) NODE_LOG_IMPL(whirl::NodeLogLevel::Debug, __VA_ARGS__)
+#define NODE_LOG_INFO(...)  NODE_LOG_IMPL(whirl::NodeLogLevel::Info, __VA_ARGS__)
+#define NODE_LOG_ERROR(...) NODE_LOG_IMPL(whirl::NodeLogLevel::Error, __VA_ARGS__)
+
+// Backward compatibility
+#define NODE_LOG(...) NODE_LOG_INFO(__VA_ARGS__)
 
 }  // namespace whirl
