bf07dd2c7983e46191ea9aec10303adb2fe559c7
Roman Lipovsky <roman.lipovsky@gmail.com>
2020-12-13 02:06:34 +0300

    Literals for jiffies

--- whirl/time.hpp
+++ whirl/time.hpp
@@ -4,11 +4,28 @@
 
 namespace whirl {
 
+//////////////////////////////////////////////////////////////////////
+
 // Time measured in some abstract `jiffies` - Paxon unit of time
 // https://lamport.azurewebsites.net/pubs/lamport-paxos.pdf, page 13, footnote 11
 
 using TimePoint = size_t;
 
 using Duration = size_t;
+
+//////////////////////////////////////////////////////////////////////
+
+// Literals for jiffies
+
+inline Duration operator""_jiffies(unsigned long long int d) {
+  return Duration{d};
+}
+
+// More compact
+inline Duration operator""_jfs(unsigned long long int d) {
+  return Duration{d};
+}
+
+//////////////////////////////////////////////////////////////////////
 
 }  // namespace whirl
