eb58e132e5debf92d3741f08c9da09bb33a981f6
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-06-13 11:47:28 +0200

    conditional -Werror, C++20

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -1,8 +1,12 @@
 # Compile options
 
-set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
 add_compile_options(-O3 -Wall -Wextra -Wpedantic -g -fno-omit-frame-pointer)
-# add_compile_options(-Werror)
+
+if(WHIRL_DEVELOPER)
+    message(STATUS "Turn warnings into errors")
+    add_compile_options(-Werror)
+endif()
