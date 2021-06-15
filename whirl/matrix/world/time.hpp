c401fecdcb5420fb5844fd61711d7f7515c949bd
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-06-16 01:36:07 +0300

    better error message

--- whirl/matrix/world/time.hpp
+++ whirl/matrix/world/time.hpp
@@ -17,7 +17,7 @@ class WorldTime {
   WorldTime& operator=(const WorldTime&) = delete;
 
   void AdvanceTo(TimePoint future) {
-    WHEELS_VERIFY(future >= now_, "Cannot move world clock backward: now = " << now_ << " -> " << future);
+    WHEELS_VERIFY(future >= now_, "Cannot move world time backward: now = " << now_ << " -> " << future);
     now_ = future;
   }
 
