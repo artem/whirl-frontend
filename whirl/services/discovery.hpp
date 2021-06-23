374170bec750230000cfbe4cc8098f6c033d6fba
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-06-23 12:04:50 +0300

    add comment

--- whirl/services/discovery.hpp
+++ whirl/services/discovery.hpp
@@ -9,6 +9,7 @@ namespace whirl {
 struct IDiscoveryService {
   virtual ~IDiscoveryService() = default;
 
+  // List of hostnames
   using Pool = std::vector<std::string>;
 
   virtual Pool GetPool(const std::string& name) = 0;
