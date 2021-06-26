b29b18154b585f8e6f09583675dab30c944af890
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-06-26 13:32:18 +0300

    Match allocator checksums in TestDeterminism

--- whirl/engines/matrix/test/main.cpp
+++ whirl/engines/matrix/test/main.cpp
@@ -1,6 +1,7 @@
 #include <whirl/engines/matrix/test/main.hpp>
 
 #include <whirl/engines/matrix/log/file.hpp>
+#include <whirl/engines/matrix/memory/new.hpp>
 
 #include <wheels/support/argparse.hpp>
 
@@ -10,11 +11,15 @@
 
 namespace whirl::matrix {
 
+//////////////////////////////////////////////////////////////////////
+
 void FailTest() {
   std::cout << "(ﾉಥ益ಥ）ﾉ ┻━┻" << std::endl;
   std::cout.flush();
   std::exit(1);
 }
+
+//////////////////////////////////////////////////////////////////////
 
 void TestDeterminism(Simulation sim) {
   static const size_t kSeed = 104107713;
@@ -22,14 +27,27 @@ void TestDeterminism(Simulation sim) {
   std::cout << "Test determinism with seed " << kSeed << ":" << std::endl;
 
   size_t digest1 = sim(kSeed);
+
+  auto allocs_checksum_1 = GlobalAllocsCheckSum();
+
   size_t digest2 = sim(kSeed);
 
+  auto allocs_checksum_2 = GlobalAllocsCheckSum();
+
   if (digest1 != digest2) {
-    std::cout << "Impl is not deterministic: digest1 = " << digest1
+    std::cerr << "Simulation is not deterministic: digest1 = " << digest1
               << ", digest2 = " << digest2 << std::endl;
     FailTest();
   }
+
+  // Simulator internal check
+  if (allocs_checksum_1 != allocs_checksum_2) {
+    std::cerr << "Global allocator checksums mismatch" << std::endl;
+    FailTest();
+  }
 }
+
+//////////////////////////////////////////////////////////////////////
 
 void RunSimulations(Simulation sim, size_t count) {
   std::mt19937 seeds{42};
@@ -50,6 +68,8 @@ void RunSimulation(Simulation sim, size_t seed) {
   std::cout << "Simulation digest = " << digest << std::endl;
 }
 
+//////////////////////////////////////////////////////////////////////
+
 static void CLI(wheels::ArgumentParser& parser) {
   parser.AddHelpFlag();
 
@@ -66,6 +86,8 @@ static T FromString(std::string str) {
   input >> value;
   return value;
 }
+
+//////////////////////////////////////////////////////////////////////
 
 int Main(int argc, const char** argv, Simulation sim) {
   wheels::ArgumentParser parser{"Whirl simulator"};
