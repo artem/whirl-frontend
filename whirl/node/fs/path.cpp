55675532147bb482b598b620b1b2fd397b0f7e06
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-09-20 00:51:20 +0300

    better

--- whirl/node/fs/path.cpp
+++ whirl/node/fs/path.cpp
@@ -9,7 +9,7 @@ Path::Path(std::string repr) : Path(GetRuntime().FileSystem(), repr) {
 }
 
 Path& Path::operator/=(const std::string& name) {
-  repr_ = fs_->PathAppend(Repr(), name);
+  repr_ = fs_->PathAppend(repr_, name);
   return *this;
 }
 
